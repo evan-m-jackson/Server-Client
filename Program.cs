4609a5edec495e9008cf82a56748abf6044e12e2
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2022-10-12 11:28:19 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace client
+{
+    public class Client
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting client...");
+
+            TcpClient client = new TcpClient("127.0.0.1", 5000);
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+
+            
+            String input = "";
+
+            while (input != "quit")
+            {
+                Console.Write("Enter text ('quit' to stop): ");
+                input = Console.ReadLine();
+                writer.WriteLine(input);
+                writer.Flush();
+
+                string lineReceived = reader.ReadLine();
+                Console.WriteLine("Received from server: " + lineReceived);
+                Console.Write("Enter text: 'quit' to stop: ");
+            }
+
+            writer.WriteLine("quit");
+            writer.Flush();
+        }
+    }
+}
